165fc46b4ed6c2018a99b2051bccca921e7d9d2b
felix <felix7051@gmail.com>
2023-11-01 20:33:31 +0100

    [Config] Add frontend given

---
+++ front-end/src/data/RecipeData.js
@@ -0,0 +1,52 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:8080/v1/';
+
+// Fonction pour obtenir toutes les recettes
+export async function getRecipes() {
+    try {
+        return await axios.get(BASE_URL + 'recipes');
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
+}
+
+// Fonction pour obtenir une recette par son ID
+export async function getRecipeById(id) {
+    try {
+        return await axios.get(BASE_URL + `recipes/${id}`);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// Fonction pour créer une nouvelle recette
+export async function createRecipe(recipeData) {
+    try {
+        const response = await axios.post(BASE_URL + 'recipes', recipeData);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// Fonction pour mettre à jour une recette existante
+export async function updateRecipe(id, recipeData) {
+    try {
+        const response = await axios.put(BASE_URL + `recipes/${id}`, recipeData);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// Fonction pour supprimer une recette par son ID
+export async function deleteRecipe(id) {
+    try {
+        const response = await axios.delete(BASE_URL + `recipes/${id}`);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+}
