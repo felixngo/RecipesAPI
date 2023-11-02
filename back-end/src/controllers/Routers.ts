ae653730c90616c5f386bb71468715fc288ca637
felix <felix7051@gmail.com>
2023-11-02 12:23:02 +0100

    [Backend] add back transform router

--- back-end/src/controllers/Routers.ts
+++ back-end/src/controllers/Routers.ts
@@ -1,7 +1,8 @@
 import {Router} from "express";
 import {recipesRouter} from "./RecipesController";
+import {transformDataRouter} from "./TransformDataController";
 
 export const router = Router();
 
 router.use('/recipes', recipesRouter);
-// router.use('/transformData', transformDataRouter);
+router.use('/transformData', transformDataRouter);
